709a6ea05e824f071a2c00bb34796478d545e773
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-08 12:42:56 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/annotation/EnableUploadImageTempStorage.java
@@ -0,0 +1,15 @@
+package net.devstudy.resume.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+
+/**
+ * Adds net.devstudy.resume.component.impl.UploadImageTempStorage around aspect for method annotated by this annotation
+ * 
+ */
+ 
+@Retention(RUNTIME)
+public @interface EnableUploadImageTempStorage {
+
+}
