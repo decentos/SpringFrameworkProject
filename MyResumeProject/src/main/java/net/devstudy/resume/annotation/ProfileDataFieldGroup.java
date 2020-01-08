61c32f2c5829daea8fb363b6b879e42c5a0fee78
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-08 12:56:04 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/annotation/ProfileDataFieldGroup.java
@@ -0,0 +1,18 @@
+package net.devstudy.resume.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+
+/**
+ * Marker annotation which groups the fields from Profile document into field list, 
+ * which should be used for updating profile data. This marker is used for updating profile document by reflection.
+ * 
+ * Please look at net.devstudy.resume.service.impl.EditProfileServiceImpl.updateProfileData() for details 
+ * 
+ */
+ 
+@Retention(RUNTIME)
+public @interface ProfileDataFieldGroup {
+
+}
