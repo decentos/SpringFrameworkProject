8dd079fb046847d7e01f0599d106e7644d352eb0
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-09 19:20:27 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/component/impl/EmulateTransactionSynchronizationSupport.java
@@ -0,0 +1,39 @@
+package net.devstudy.resume.component.impl;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+/**
+ * Emulates TransactionSynchronization support for not transactional environment
+ */
+@Aspect
+@Component
+public class EmulateTransactionSynchronizationSupport {
+
+	@Around("@annotation(org.springframework.transaction.annotation.Transactional)")
+	public Object advice(ProceedingJoinPoint pjp) throws Throwable {
+		TransactionSynchronizationManager.initSynchronization();
+		try {
+			Object result = pjp.proceed();
+			for(TransactionSynchronization transactionSynchronization : TransactionSynchronizationManager.getSynchronizations()) {
+				transactionSynchronization.afterCommit();
+			}
+			return result;
+		} catch (Exception e) {
+			for(TransactionSynchronization transactionSynchronization : TransactionSynchronizationManager.getSynchronizations()) {
+				if(e instanceof RuntimeException) {
+					transactionSynchronization.afterCompletion(TransactionSynchronization.STATUS_ROLLED_BACK);
+				} else {
+					transactionSynchronization.afterCommit();
+				}
+			}
+			throw e;
+		} finally {
+			TransactionSynchronizationManager.clearSynchronization();
+		}
+	}
+}
