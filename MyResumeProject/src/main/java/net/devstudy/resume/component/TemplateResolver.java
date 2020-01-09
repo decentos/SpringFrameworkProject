aef23378de83e2b06f8f817203157da80484f9d6
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-09 19:53:34 +0300

    JPA implementation of My resume application

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/component/TemplateResolver.java
@@ -0,0 +1,9 @@
+package net.devstudy.resume.component;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public interface TemplateResolver {
+
+	@Nonnull String resolve(@Nonnull String template, @Nullable Object model);
+}
