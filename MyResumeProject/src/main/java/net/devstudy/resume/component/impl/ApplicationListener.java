ff453dbd449b2e5ea47f39137001eca5e72b8f7d
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-09 19:18:07 +0300

    JPA implementation of My resume application

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/component/impl/ApplicationListener.java
@@ -0,0 +1,52 @@
+package net.devstudy.resume.component.impl;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationListener implements ServletContextListener {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationListener.class);
+
+	@Value("${application.production}")
+	private boolean production;
+	
+	@Value("${application.css.common.version}")
+	private String cssCommonVersion;
+	
+	@Value("${application.css.ex.version}")
+	private String cssExVersion;
+	
+	@Value("${application.js.common.version}")
+	private String jsCommonVersion;
+	
+	@Value("${application.js.ex.version}")
+	private String jsExVersion;
+	
+	@Value("${application.js.messages.version}")
+	private String jsMessagesVersion;
+	
+	@Value("${application.host}")
+	private String applicationHost;
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		sce.getServletContext().setAttribute("production", 		 production);
+		sce.getServletContext().setAttribute("cssCommonVersion", cssCommonVersion);
+		sce.getServletContext().setAttribute("cssExVersion", 	 cssExVersion);
+		sce.getServletContext().setAttribute("jsCommonVersion",  jsCommonVersion);
+		sce.getServletContext().setAttribute("jsExVersion", 	 jsExVersion);
+		sce.getServletContext().setAttribute("jsMessagesVersion",jsMessagesVersion);
+		sce.getServletContext().setAttribute("applicationHost",  applicationHost);
+		LOGGER.info("Application started");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		LOGGER.info("Application stopped");
+	}
+}
