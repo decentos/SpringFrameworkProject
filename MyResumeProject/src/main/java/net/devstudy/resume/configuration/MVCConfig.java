9f0e1bd328ef9e775e7c845d302c9e93ac97a4d1
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-10 19:38:31 +0300

    JPA implementation of My resume application

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/configuration/MVCConfig.java
@@ -0,0 +1,77 @@
+package net.devstudy.resume.configuration;
+
+import java.util.List;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import net.devstudy.resume.component.impl.ErrorHandler;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "net.devstudy.resume.controller"})
+@EnableSpringDataWebSupport
+public class MVCConfig extends WebMvcConfigurerAdapter {
+	
+	@Autowired
+	private ErrorHandler errorHandler;
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/JSP/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver(){
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		return multipartResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+		registry.addResourceHandler("/media/**").addResourceLocations("/media/");
+		registry.addResourceHandler("/favicon.ico").addResourceLocations("/favicon.ico");
+		registry.addResourceHandler("/browserconfig.xml").addResourceLocations("/browserconfig.xml");
+	}
+	
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+		exceptionResolvers.add(errorHandler);
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean localValidatorFactoryBean() {
+	    LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+	    localValidatorFactoryBean.setProviderClass(HibernateValidator.class);
+	    localValidatorFactoryBean.setValidationMessageSource(messageSource());
+	    return localValidatorFactoryBean;
+	}
+	
+	@Bean
+    public MessageSource messageSource() { 
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n.messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
