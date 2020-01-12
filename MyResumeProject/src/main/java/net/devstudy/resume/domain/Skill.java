407c790a80b23e3ab10887566e6d308692023af9
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-12 14:27:56 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/domain/Skill.java
@@ -0,0 +1,93 @@
+package net.devstudy.resume.domain;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.SafeHtml;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import net.devstudy.resume.annotation.constraints.EnglishLanguage;
+
+public class Skill implements Serializable, ProfileCollectionField, Comparable<Skill> {
+	private static final long serialVersionUID = 1L;
+
+	private Short idCategory;
+
+	@JsonIgnore
+	@SafeHtml
+	@EnglishLanguage(withSpechSymbols = false, withNumbers = false)
+	private String category;
+
+	@SafeHtml
+	@EnglishLanguage
+	private String value;
+
+	public Skill() {
+	}
+
+	public String getCategory() {
+		return this.category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Short getIdCategory() {
+		return idCategory;
+	}
+
+	public void setIdCategory(Short idCategory) {
+		this.idCategory = idCategory;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((idCategory == null) ? 0 : idCategory.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof Skill))
+			return false;
+		Skill other = (Skill) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (idCategory == null) {
+			if (other.idCategory != null)
+				return false;
+		} else if (!idCategory.equals(other.idCategory))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Skill o) {
+		return getIdCategory().compareTo(o.getIdCategory());
+	}
+}
