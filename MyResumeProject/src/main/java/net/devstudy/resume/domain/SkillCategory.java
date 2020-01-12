cffdb851a0166815797100084c6f9f687c2cca12
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-12 14:28:30 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/domain/SkillCategory.java
@@ -0,0 +1,49 @@
+package net.devstudy.resume.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="skillCategory")
+public class SkillCategory extends AbstractDocument<String>{
+	private static final long serialVersionUID = -8959739023562086833L;
+	public static final String ORDER_FIELD_NAME = "idCategory"; 
+	@Id
+	private String id;
+	
+	private Short idCategory;
+	
+	private String category;	
+	
+	public SkillCategory() {
+		super();
+	}
+
+	public SkillCategory(String category) {
+		super();
+		this.category = category;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Short getIdCategory() {
+		return idCategory;
+	}
+
+	public void setIdCategory(Short idCategory) {
+		this.idCategory = idCategory;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
