50cebd41a314a97fb1b29cc926a704b31670d999
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-12 14:27:18 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/domain/RememberMeToken.java
@@ -0,0 +1,30 @@
+package net.devstudy.resume.domain;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
+
+@Document(collection="rememberMeToken")
+public class RememberMeToken extends PersistentRememberMeToken {
+
+	@Id
+    private String id;
+	
+	@PersistenceConstructor
+	public RememberMeToken(String id, String username, String series, String tokenValue, Date date) {
+		super(username, series, tokenValue, date);
+		this.id = id;
+	}
+	
+	public RememberMeToken(PersistentRememberMeToken token) {
+		super(token.getUsername(), token.getSeries(), token.getTokenValue(), token.getDate());
+		this.id = null;
+	}
+	
+	public String getId() {
+		return id;
+	}
+}
