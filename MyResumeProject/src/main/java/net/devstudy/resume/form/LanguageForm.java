ab84fbda7c1128a3f22f129c9981ce050ecc9a69
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-13 19:39:26 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/form/LanguageForm.java
@@ -0,0 +1,30 @@
+package net.devstudy.resume.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import net.devstudy.resume.domain.Language;
+
+public class LanguageForm {
+	@Valid
+	private List<Language> items = new ArrayList<>();
+	
+	public LanguageForm() {
+		super();
+	}
+
+	public LanguageForm(List<Language> items) {
+		super();
+		this.items = items;
+	}
+
+	public List<Language> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Language> items) {
+		this.items = items;
+	}
+}
