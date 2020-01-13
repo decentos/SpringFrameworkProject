06af887534fc8db26521656d4cfd9b77dacc186a
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-13 19:38:20 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/form/EducationForm.java
@@ -0,0 +1,30 @@
+package net.devstudy.resume.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import net.devstudy.resume.domain.Education;
+
+public class EducationForm {
+	@Valid
+	private List<Education> items = new ArrayList<>();
+	
+	public EducationForm() {
+		super();
+	}
+
+	public EducationForm(List<Education> items) {
+		super();
+		this.items = items;
+	}
+
+	public List<Education> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Education> items) {
+		this.items = items;
+	}
+}
