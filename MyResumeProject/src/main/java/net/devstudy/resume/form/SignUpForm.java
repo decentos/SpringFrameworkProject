ce5c69018cb517755feecc6c38ee338841c19747
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-13 19:41:39 +0300

    JPA implementation of My resume application

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/form/SignUpForm.java
@@ -0,0 +1,39 @@
+package net.devstudy.resume.form;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.SafeHtml;
+
+import net.devstudy.resume.annotation.constraints.EnglishLanguage;
+
+public class SignUpForm extends PasswordForm {
+
+	@NotNull
+	@Size(max=50)
+	@SafeHtml
+	@EnglishLanguage(withNumbers=false, withSpechSymbols=false)
+	private String firstName;
+
+	@NotNull
+	@Size(max=50)
+	@SafeHtml
+	@EnglishLanguage(withNumbers=false, withSpechSymbols=false)
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
