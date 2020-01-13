8ab356b2bf894a6ae0ceba06d7e58b60d6bf7287
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-13 19:38:55 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/form/InfoForm.java
@@ -0,0 +1,35 @@
+package net.devstudy.resume.form;
+
+import org.hibernate.validator.constraints.SafeHtml;
+
+import net.devstudy.resume.annotation.constraints.EnglishLanguage;
+import net.devstudy.resume.domain.Profile;
+
+public class InfoForm {
+
+	@EnglishLanguage
+	@SafeHtml
+	private String info;
+
+	public InfoForm() {
+		super();
+	}
+
+	public InfoForm(String info) {
+		super();
+		this.info = info;
+	}
+	
+	public InfoForm(Profile profile) {
+		super();
+		this.info = profile.getInfo();
+	}
+
+	public String getInfo() {
+		return info != null ? info.trim() : null;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+}
