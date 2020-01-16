05ae50f190232a858586fa82136d25a833a8d720
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-16 20:43:22 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/repository/search/ProfileSearchRepository.java
@@ -0,0 +1,19 @@
+package net.devstudy.resume.repository.search;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import net.devstudy.resume.domain.Profile;
+
+public interface ProfileSearchRepository extends ElasticsearchRepository<Profile, String> {
+
+	/**
+	 * 
+	 * http://docs.spring.io/spring-data/elasticsearch/docs/current/reference/html/#elasticsearch.query-methods.criterions
+	 */
+	Page<Profile> findByObjectiveLikeOrSummaryLikeOrInfoLikeOrCertificatesNameLikeOrLanguagesNameLikeOrPracticsCompanyLikeOrPracticsPositionLikeOrPracticsResponsibilitiesLikeOrSkillsValueLike(
+			String objective, String info, String summary, String certificateName, String languageName, String practicCompany, 
+			String practicPosition, String practicResponsibility, String skillValue, Pageable pageable);
+	
+}
