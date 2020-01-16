f9c7ac860f7a178c51495a2332ee4c441c1a7cfd
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-16 20:38:46 +0300

    Mongo db implementation for My Resume application

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/model/LanguageType.java
@@ -0,0 +1,41 @@
+package net.devstudy.resume.model;
+
+import java.beans.PropertyEditorSupport;
+
+import net.devstudy.resume.util.DataUtil;
+
+public enum LanguageType {
+
+	ALL,
+
+	SPOKEN,
+
+	WRITING;
+
+	public String getCaption() {
+		return DataUtil.capitalizeName(name());
+	}
+	
+	public String getDbValue() {
+		return name();
+	}
+
+	public LanguageType getReverseType() {
+		if (this == SPOKEN) {
+			return WRITING;
+		} else if (this == WRITING) {
+			return SPOKEN;
+		} else {
+			throw new IllegalArgumentException(this+" does not have reverse type");
+		}
+	}
+	
+	public static PropertyEditorSupport getPropertyEditor(){
+		return new PropertyEditorSupport(){
+			@Override
+			public void setAsText(String dbValue) throws IllegalArgumentException {
+				setValue(LanguageType.valueOf(dbValue.toUpperCase()));
+			}
+		};
+	}
+}
