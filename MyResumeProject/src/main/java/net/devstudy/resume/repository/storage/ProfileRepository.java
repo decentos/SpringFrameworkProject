8151cd994f219f866c99823bbad736e8a3cdc7e9
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-18 11:59:45 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/repository/storage/ProfileRepository.java
@@ -0,0 +1,27 @@
+package net.devstudy.resume.repository.storage;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import net.devstudy.resume.domain.Profile;
+
+public interface ProfileRepository extends PagingAndSortingRepository<Profile, String> {
+
+	Profile findByUid(String uid);
+	
+	Profile findByEmail(String email);
+	
+	Profile findByPhone(String phone);
+	
+	Profile findByUidOrEmailOrPhone(String uid, String email, String phone);
+	
+	int countByUid(String uid);
+	
+	Page<Profile> findAllByCompletedTrue(Pageable pageable);
+	
+	List<Profile> findByCompletedFalseAndCreatedBefore(Date created);
+}
