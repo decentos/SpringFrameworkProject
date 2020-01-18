5cec0044654d254715d2cbe0b097be2e10e9a401
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-18 12:04:28 +0300

    Mongo db implementation for My Resume application

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/util/DebugUtil.java
@@ -0,0 +1,18 @@
+package net.devstudy.resume.util;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+
+public class DebugUtil {
+	public static void turnOnShowMongoQuery() {
+		Logger sqlLogger = (Logger) LoggerFactory.getLogger("org.springframework.data.mongodb.core.MongoTemplate");
+		sqlLogger.setLevel(Level.DEBUG);
+	}
+
+	public static void turnOffShowMongoQuery() {
+		Logger sqlLogger = (Logger) LoggerFactory.getLogger("org.springframework.data.mongodb.core.MongoTemplate");
+		sqlLogger.setLevel(Level.INFO);
+	}
+}
