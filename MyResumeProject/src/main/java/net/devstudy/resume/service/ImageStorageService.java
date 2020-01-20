d982c72cc6f01e896442ca87b57e5f9458e10d28
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-20 21:14:21 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/service/ImageStorageService.java
@@ -0,0 +1,17 @@
+package net.devstudy.resume.service;
+
+import java.nio.file.Path;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.devstudy.resume.Constants.UIImageType;
+
+public interface ImageStorageService {
+	
+	@Nonnull String createImageLink (@Nonnull String imageName, @Nonnull UIImageType imageType);
+	
+	void save(@Nonnull String imageLink, @Nonnull Path tempImageFile);
+
+	void remove (@Nullable String ... imageLinks);
+}
