8e44c23d15585cb66dad136061cdc829179456f9
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-01-20 21:12:47 +0300

    Clear code

---
+++ MyResumeProject/src/main/java/net/devstudy/resume/service/FindProfileService.java
@@ -0,0 +1,24 @@
+package net.devstudy.resume.service;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import net.devstudy.resume.domain.Profile;
+
+public interface FindProfileService {
+
+	@Nonnull Page<Profile> findAll(@Nonnull Pageable pageable);
+
+	@Nullable Profile findByUid(@Nonnull String uid);
+
+	@Nonnull Page<Profile> findBySearchQuery(@Nonnull String query, @Nonnull Pageable pageable);
+
+	void restoreAccess(@Nonnull String anyUnigueId);
+	
+	@Nullable Profile findByRestoreToken(@Nonnull String token);
+	
+	@Nonnull Iterable<Profile> findAllForIndexing();
+}
