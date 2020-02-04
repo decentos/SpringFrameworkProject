c0537101771046d149c3290bc991cb6133606b5b
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-04 14:14:25 +0300

    Spring Annotations Part 2

---
+++ SpringCourseByAlishev/Lesson10/SpringAnnotationsPart2/src/main/java/ru/alishev/springcourse/RockMusic.java
@@ -0,0 +1,11 @@
+package ru.alishev.springcourse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
