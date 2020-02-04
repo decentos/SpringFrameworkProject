9e4234dec72e5add862ecc6f2939807ceb312588
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-04 14:10:41 +0300

    Spring Annotations Part 2

---
+++ SpringCourseByAlishev/Lesson10/SpringAnnotationsPart2/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -0,0 +1,11 @@
+package ru.alishev.springcourse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
