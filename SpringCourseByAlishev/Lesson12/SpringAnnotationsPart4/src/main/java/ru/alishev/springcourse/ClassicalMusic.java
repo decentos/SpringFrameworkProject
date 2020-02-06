18b5d5ec71dae51afee6284101eed0c9dfd3be45
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-06 17:20:43 +0300

    Spring Annotations Part 4

---
+++ SpringCourseByAlishev/Lesson12/SpringAnnotationsPart4/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -0,0 +1,28 @@
+package ru.alishev.springcourse;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+@Scope("prototype")
+public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    // Для Prototype бинов не вызывается destroy-метод!
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
