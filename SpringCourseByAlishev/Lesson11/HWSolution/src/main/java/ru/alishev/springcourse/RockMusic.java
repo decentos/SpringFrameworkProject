98acb88ec4427cd72e904bcea05ac41580b4c821
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-06 17:14:06 +0300

    Home work solution

---
+++ SpringCourseByAlishev/Lesson11/HWSolution/src/main/java/ru/alishev/springcourse/RockMusic.java
@@ -0,0 +1,24 @@
+package ru.alishev.springcourse;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class RockMusic implements Music {
+    private List<String> songs = new ArrayList<>();
+
+    // Блок инициализации объекта (англ. Instance initialization block)
+    // Выполняется каждый раз, когда создается объект класса
+    {
+        songs.add("Wind cries Mary");
+        songs.add("Paint it black");
+        songs.add("Can't seem to make you mine");
+    }
+
+    @Override
+    public List<String> getSongs() {
+        return songs;
+    }
+}
