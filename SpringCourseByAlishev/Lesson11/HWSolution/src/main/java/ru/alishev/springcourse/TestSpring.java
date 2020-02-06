de196b8ffa6bba8a7f995d4df15095f03eb60155
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-06 17:14:52 +0300

    Home work solution

---
+++ SpringCourseByAlishev/Lesson11/HWSolution/src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -0,0 +1,18 @@
+package ru.alishev.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic(MusicGenre.CLASSICAL);
+        musicPlayer.playMusic(MusicGenre.ROCK);
+
+        context.close();
+    }
+}
