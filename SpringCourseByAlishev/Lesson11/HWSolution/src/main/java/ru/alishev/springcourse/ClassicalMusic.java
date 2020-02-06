878fc9b5251e0b8a353045b0805ed7b68ed9bc40
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-06 17:10:09 +0300

    Home work solution

---
+++ SpringCourseByAlishev/Lesson11/HWSolution/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -0,0 +1,24 @@
+package ru.alishev.springcourse;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ClassicalMusic implements Music {
+    private List<String> songs = new ArrayList<>();
+
+    // Блок инициализации объекта (англ. Instance initialization block)
+    // Выполняется каждый раз, когда создается объект класса
+    {
+        songs.add("Hungarian Rhapsody");
+        songs.add("Symphony no. 5 in C Minor, op. 67");
+        songs.add("Night on Bald Mountain");
+    }
+
+    @Override
+    public List<String> getSongs() {
+        return songs;
+    }
+}
