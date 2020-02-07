bc45bfdedb432ebd1438626d6cd6580cdab77f49
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-07 15:00:16 +0300

    Home work solution

---
+++ SpringCourseByAlishev/Lesson13/HWSolution/src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -0,0 +1,18 @@
+package ru.alishev.springcourse;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.alishev.springcourse.config.SpringConfig;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.playMusic());
+
+        context.close();
+    }
+}
