068aeac795a7d99dfc1b8f3b9274853ed9a03191
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-07 14:57:26 +0300

    Home work solution

---
+++ SpringCourseByAlishev/Lesson13/HWSolution/src/main/java/ru/alishev/springcourse/genres/ClassicalMusic.java
@@ -0,0 +1,25 @@
+package ru.alishev.springcourse.genres;
+
+import ru.alishev.springcourse.Music;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    // Для Prototype бинов не вызывается destroy-метод!
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
